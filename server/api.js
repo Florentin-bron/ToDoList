5ebc8924120ccc8babcfb1b404e13f89feee593f
florentin <florentin.bron@gmail.com>
2022-03-15 16:22:00 +0100

    ajout api

---
+++ server/api.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const cors = require("cors");
+const app = express()
+/* const port = process.env.PORT || 8000 */
+const mysql      = require('mysql');
+const {forEach} = require("react-bootstrap/ElementChildren");
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    database: 'todo',
+    user     : 'root',
+    password : 'root',
+    dateStrings: 'date'
+});
+
+app.listen(4567, () => {})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/todos', (req, res) => {
+    connection.query(`SELECT * FROM todo`, function(err, rows, fields) {
+        res.json(rows)
+    });
+})
+
+app.post('/api/todos', (req, res) =>{
+    const { titre } = req.query;
+    const { description } = req.query;
+    const { date } = req.query;
+    const { label } = req.query;
+    const today = new Date();
+    const creationdate = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+    if(!titre || !description || !date || !label){
+        res.status(400).json(
+            { error: 'one or more parameters is required!' }
+        )
+    }
+    connection.query(
+            `INSERT INTO todo (\`id\`, \`titre\`, \`description\`, \`due_date\`, \`label_id\`, \`creation_date\`)
+            VALUES (NULL, '${titre}', '${description}', '${date}', '${label}', '${creationdate}');`,
+        function(err, rows, fields) {
+        res.json({rows})
+    });
+});
+
+
+app.patch('/api/todos', (req, res ) =>{
+    const { id } = req.query;
+    const { body } = req;
+    if(!id || !body){
+        res.status(400).json(
+            { error: 'one or more parameters is required!' }
+        )
+    }
+
+    let setString = ""
+    for (const [key, value] of Object.entries(body)) {
+        if(setString === ""){
+            setString += `\`${key}\` = \'${value}\'`;
+        }
+        else {
+            setString += `, \`${key}\` = \'${value}\'`;
+        }
+    }
+        connection.query(`UPDATE todo SET ${setString} WHERE \`todo\`.\`id\` = ${id};`,
+        function(err, rows, fields) {
+            res.json({rows})
+        });
+});
+
+app.delete('/api/todos', (req, res ) =>{
+    const { id } = req.query;
+    if(!id){
+        res.status(400).json(
+            { error: 'id parameters is required!' }
+        )
+    }
+    connection.query(`DELETE FROM todo WHERE \`todo\`.\`id\` = ${id}`,
+        function(err, rows, fields) {
+            res.json({rows})
+        });
+});
